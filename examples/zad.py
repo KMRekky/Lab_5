1ddf007fbb848b0648f2230c25374d9fb8744dce
KMRekky <maciek.szreder4444@gmail.com>
2025-05-29 18:35:45 +0200

    Add files via upload

---
+++ examples/zad.py
@@ -0,0 +1,26 @@
+from PIL import Image
+img = Image.open('test.jpg')
+img.show()
+
+resized = img.resize((2000, 1000))
+resized.save('test.jpg')
+
+from PIL import Image, ImageFilter
+img = Image.open("test.jpg")
+blurred = img.filter(ImageFilter.GaussianBlur(5))
+blurred.save("test_blurred.jpg")
+gray = img.convert("L")
+gray.save("test_gray.jpg")
+
+import cv2
+img = cv2.imread("test.jpg", cv2.IMREAD_GRAYSCALE)
+edges = cv2.Canny(img, 100, 200)
+cv2.imwrite("test_edges.jpg", edges)
+
+import cv2
+img = cv2.imread("test.jpg")
+(h, w) = img.shape[:2]
+center = (w // 2, h // 2)
+M = cv2.getRotationMatrix2D(center, 45, 1.0)
+rotated = cv2.warpAffine(img, M, (w, h))
+cv2.imwrite("test_rotated.jpg", rotated)
